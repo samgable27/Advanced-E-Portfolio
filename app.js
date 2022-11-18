cfbab4b51a798d0e9909aa7d60c45531a233d817
Sam <samarooniii@yahoo.com>
2022-11-18 01:44:11 -0500

    new portolfio

---
+++ app.js
@@ -0,0 +1,37 @@
+// template_z1hamlq
+
+{
+  /* <script type="text/javascript"
+        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js">
+</script>
+<script type="text/javascript">
+   (function(){
+      emailjs.init(gfzbFYRTADe_uMBHq);
+   })();
+</script> */
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_joft5u7",
+      "template_z1hamlq",
+      event.target,
+      "gfzbFYRTADe_uMBHq"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+      );
+    });
+}
